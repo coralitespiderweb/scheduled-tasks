58f5f0c465777a8591cf8e7b456e8b8688fc6c4e
duskyweb <jenniferzhou3413@gmail.com>
2026-08-04 11:21:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ if today in birthdays_dict:
         content = file.read()
 
     content = content.replace("[NAME]", birthdays_dict[today].split(",")[0])
+else:
+  pass
 
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
